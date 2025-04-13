a08a6113eda99af044a38a80b365193255dd120b
FelipeJaydenX <felipehiro42699@gmail.com>
2025-04-12 21:22:50 -0300

    Create sleep.c

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    fprintf(2, "uso: sleep ticks\n");
+    exit(1);
+  }
+
+  int ticks = atoi(argv[1]);
+  if (ticks < 0) {
+    fprintf(2, "sleep: número de ticks invalido\n");
+    exit(1);
+  }
+
+  sleep(ticks);
+  exit(0);
+}
